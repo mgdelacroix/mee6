b4728972b30c320d88327254428922331a06063a
Miguel de la Cruz <miguel@mcrx.me>
2017-07-11 19:36:03 +0200

    Add basic implementation of the service module

--- src/hal/modules/service.clj
+++ src/hal/modules/service.clj
@@ -1,12 +1,18 @@
-(ns hal.modules.service)
+(ns hal.modules.service
+  (:require [hal.ssh :as ssh]))
+
+(defn get-last-log
+  [host service]
+  (:out (ssh/run host (str "journalctl -r -n 10 -u " service))))
 
 (defn run
-  [session ctx]
-  {:status :ok  ;; :ko, out of systemctl status
-   :last-log ["" "" ""]})  ;; last lines of the journalctl
+  [{:keys [host service] :as ctx}]
+  (let [{:keys [exit] :as result} (ssh/run host (str "systemctl status " service))]
+    {:status (if (= exit 0) :up :down)
+     :last-log (get-last-log host service)}))
 
 (defn check
-  [{:keys [status]} ctx]
-  (if (= status :ok)
+  [ctx {:keys [status] :as result}]
+  (if (= status :up)
     :green
     :red))
