6d970483e70793b7c9a16b6987a778f83d2695f8
Miguel de la Cruz <miguel@mcrx.me>
2017-07-13 11:40:26 +0200

    Add script module

---
+++ src/hal/modules/script.clj
@@ -0,0 +1,43 @@
+(ns hal.modules.script
+  (:require [hal.ssh :as ssh]
+            [clojure.java.io :as io]
+            [cuerdas.core :as str]))
+
+(defn get-basename
+  [path]
+  (-> (io/file path)
+      (.getName)))
+
+(defn get-remote-path
+  [local-path]
+  (let [filename (get-basename local-path)]
+    (str "/tmp/.mee6-" filename)))
+
+(defn process-out
+  [{:keys [exit out] :as result}]
+  (letfn [(reduce-output [acc val]
+            (if (or (= val "---") (not (empty? acc)))
+              (conj acc val)
+              acc))
+          (transform-to-map [acc val]
+            (let [[key value] (map str/trim (str/split val #":" 2))
+                  key (keyword "script" key)]
+              (assoc acc key value)))]
+    (->> (str/lines out)
+         (reduce reduce-output [])
+         (rest)
+         (reduce transform-to-map result))))
+
+(defn run
+  [{:keys [host file args] :as ctx}]
+  (let [remote-path (get-remote-path file)
+        _ (ssh/copy host file remote-path)
+        {:keys [exit] :as result} (ssh/run host (str remote-path " " args))]
+    (process-out result)))
+
+(defn check
+  [ctx {:keys [exit out err] :as result}]
+  (case exit
+    0 :green
+    1 :red
+    :grey))
--- src/hal/ssh.clj
+++ src/hal/ssh.clj
@@ -4,5 +4,17 @@
 
 (defn run
   [{:keys [hostname key] :as host} cmd]
+  ;; TODO: add spec for host
+  ;;       handle key
   (apply shell/sh "timeout" "5" "ssh"
          hostname (str/split cmd #"\s+")))
+
+(defn copy
+  [{:keys [hostname] :as host} local-path remote-path]
+  ;; TODO: add spec for host
+  ;;       handle key
+  (let [res (shell/sh "scp" local-path (str hostname ":" remote-path))
+        exit (:exit res)]
+    (when-not (= exit 0)
+      (throw (ex-info (str "File " local-path " failed to copy to " hostname) res)))
+  remote-path))
